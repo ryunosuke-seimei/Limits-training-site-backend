80653cfbd71169b3a3dbbfa3b3f93c1c99f49c80
ryunosuke_mac <ryunosuke0seimei@gmail.com>
2020-12-25 17:37:19 +0900

    check

--- app.py
+++ app.py
@@ -18,7 +18,8 @@ def hello_world():
 @app.route('/rin_jin/index/')
 def index():
     print(app.config["SERVER"])
-    db_connection = db.connect(host=app.config["HOST"], user=app.config["USER"], password=app.config["PASSWORD"], database=app.config["DATABASES"])
+    db_connection = db.connect(host=app.config["HOST"], user=app.config["USER"], password=app.config["PASSWORD"],
+                               database=app.config["DATABASES"])
     cursor = db_connection.cursor()
     cursor.execute(
         "select * from word_table")
@@ -35,9 +36,18 @@ def get_some_path():
 @app.route("/rin_jin/insert/", methods=["POST"])
 def insert_db():
     name = request.form["name"]
-    sentences = request.form["sentences"]
-    print(name)
-    print(sentences)
+    words = [name]
+    for number in range(10):
+        words.append(request.form["form{}".format(number)])
+    join_point = ",".join(words)
+    data_format = ",".join(["%s"] * 11)
+    db_connection = db.connect(host=app.config["HOST"], user=app.config["USER"], password=app.config["PASSWORD"],
+                               database=app.config["DATABASES"])
+    cursor = db_connection.cursor()
+    cursor.execute(
+        "INSERT INTO word_table(name,flag0,flag1,flag2,flag3,flag4,flag5,flag6,flag7,flag8,flag9) values({})".format(
+            data_format), tuple(join_point.split(",")))
+    db_connection.commit()
     # インサート
     return redirect("https://reina-raft.xyz/rin_jin/index/", code=302)
 
@@ -46,36 +56,50 @@ def insert_db():
 def edit_show(Id):
     print(Id)
     ID = int(Id)
-    db_connection = db.connect(host=app.config["HOST"], user=app.config["USER"], password=app.config["PASSWORD"], database=app.config["DATABASES"])
+    db_connection = db.connect(host=app.config["HOST"], user=app.config["USER"], password=app.config["PASSWORD"],
+                               database=app.config["DATABASES"])
     cursor = db_connection.cursor()
     cursor.execute(
         "select * from word_table where id ={}".format(ID))
     word_table = cursor.fetchone()
+    print(word_table)
     return render_template("form.html", word_table=word_table)
 
 
 @app.route("/rin_jin/update/", methods=["POST"])
 def update_db():
-    db_connection = db.connect(host=app.config["HOST"], user=app.config["USER"], password=app.config["PASSWORD"], database=app.config["DATABASES"])
+    db_connection = db.connect(host=app.config["HOST"], user=app.config["USER"], password=app.config["PASSWORD"],
+                               database=app.config["DATABASES"])
     ID = request.form["id"]
     name = request.form["name"]
-    sentences = request.form["sentences"]
+    words = [name]
+    for number in range(10):
+        words.append(request.form["form{}".format(number)])
+    data_format = ""
+    for number in range(len(words)):
+        if number == 0:
+            data_format += "name='" + words[number] + "'"
+        else:
+            data_format += ",flag{}='".format(str(number - 1)) + words[number] + "' "
 
-    # cursor = db_connection.cursor()
-    # cursor.execute(
-    #     "update word_table set  where id ={}".format(ID))
+    cursor = db_connection.cursor()
+    cursor.execute(
+        "update word_table set {} where id ={}".format(data_format, ID))
+    db_connection.commit()
 
     return redirect("https://reina-raft.xyz/rin_jin/index/", code=302)
 
 
 @app.route("/rin_jin/delete/", methods=["POST"])
 def delete_db():
-    db_connection = db.connect(host=app.config["HOST"], user=app.config["USER"], password=app.config["PASSWORD"], database=app.config["DATABASES"])
+    db_connection = db.connect(host=app.config["HOST"], user=app.config["USER"], password=app.config["PASSWORD"],
+                               database=app.config["DATABASES"])
     ID = request.form["id"]
     print(ID)
-    # cursor = db_connection.cursor()
-    # cursor.execute(
-    #     "update word_table set  where id ={}".format(ID))
+    cursor = db_connection.cursor()
+    cursor.execute(
+        "update word_table set delete_flag=TRUE where id ={}".format(ID))
+    db_connection.commit()
 
     return redirect("https://reina-raft.xyz/rin_jin/index/", code=302)
 
