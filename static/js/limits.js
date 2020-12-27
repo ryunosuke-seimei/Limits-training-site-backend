7d8745ba90d0464dfe680011ad80014cc7ccc61a
ryunosuke_mac <ryunosuke0seimei@gmail.com>
2020-12-27 17:09:47 +0900

    checked

---
+++ static/js/limits.js
@@ -0,0 +1,24 @@
+let flag = 0;
+window.onload=function(){
+    $(".number").on( "click", function () {
+        // console.log($($(this).children("input")[0]).val());
+
+        let target =$($(this).children(".sentence"));
+        let span = target.children("span");
+        let name = $(span[1]).text();
+        for (let i=2;i<span.length;i++){
+            console.log($(span[i]).text());
+        }
+
+        if(flag===0){
+            $(".target_left").text(name);
+        }else {
+            $(".target_right").text(name);
+        }
+
+    }
+)
+}
+function flag_check() {
+    flag = parseInt($('input[name="flag"]:checked').val());
+}
